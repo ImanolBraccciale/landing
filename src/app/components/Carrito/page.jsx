621b5b221427197b13da29c94c762c77cbb54ecf
ImanolBraccciale <imanol11699@outlook.com>
2024-01-21 04:42:16 -0300

    eeeeeeeeeeeee

--- src/app/components/Carrito/page.jsx
+++ src/app/components/Carrito/page.jsx
@@ -1,9 +1,9 @@
-/* @jsxImportSource client */
+"use client"
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { actualizarCantidad, enviarPorWhatsApp, handleClickOutside } from '../utils/carrito';
 
-const Carrito = ({ carrito, toggleCarrito }) => {
+export function Carrito({ carrito, toggleCarrito }) {
   const [carritoActualizado, setCarrito] = useState(carrito);
   const menuWSP = {
     mensaje: `¡Hola! Quiero hacer un pedido:\n${carritoActualizado
@@ -25,39 +25,39 @@ const Carrito = ({ carrito, toggleCarrito }) => {
 
   return (
     <div id="carritoContainer" className="position-fixed top-50 start-50 translate-middle p-5 bg-light shadow-lg" style={{ zIndex: 9, borderRadius: '15px' }}>
-    <button
-      className="btn btn-danger position-absolute top-0 end-0 m-3 rounded-circle"
-      onClick={toggleCarrito}
-    >
-      X
-    </button>
-    <h2 className="mb-4">Carrito de Compras</h2>
-    <ul className="list-group mb-3">
-      {carritoActualizado.map((item, index) => (
-        <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-          <span>{item.nombre}</span>
-          <div className="btn-group">
-            <button
-              className="btn btn-outline-secondary"
-              onClick={() => handleActualizarCantidad(item, 'restar')}
-            >
-              -
-            </button>
-            <span className="btn btn-light">{item.cantidad}</span>
-            <button
-              className="btn btn-outline-secondary"
-              onClick={() => handleActualizarCantidad(item, 'sumar')}
-            >
-              +
-            </button>
-          </div>
-        </li>
-      ))}
-    </ul>
-    <button className="btn btn-success" onClick={handleEnviarPorWhatsApp}>
-      Enviar por WhatsApp
-    </button>
-  </div>
+      <button
+        className="btn btn-danger position-absolute top-0 end-0 m-3 rounded-circle"
+        onClick={toggleCarrito}
+      >
+        X
+      </button>
+      <h2 className="mb-4">Carrito de Compras</h2>
+      <ul className="list-group mb-3">
+        {carritoActualizado.map((item, index) => (
+          <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+            <span>{item.nombre}</span>
+            <div className="btn-group">
+              <button
+                className="btn btn-outline-secondary"
+                onClick={() => handleActualizarCantidad(item, 'restar')}
+              >
+                -
+              </button>
+              <span className="btn btn-light">{item.cantidad}</span>
+              <button
+                className="btn btn-outline-secondary"
+                onClick={() => handleActualizarCantidad(item, 'sumar')}
+              >
+                +
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <button className="btn btn-success" onClick={handleEnviarPorWhatsApp}>
+        Enviar por WhatsApp
+      </button>
+    </div>
   );
 };
 
