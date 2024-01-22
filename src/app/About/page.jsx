8b771119194c9bba38ee2564d477c35f5256b8b9
ImanolBraccciale <imanol11699@outlook.com>
2024-01-22 14:52:39 -0300

    Abouts

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -2,30 +2,26 @@
 
 import Image from "next/image"
 import "./About.css"
-export default function About (){
+export default function About() {
 
-    
+
     return (
-        <section>
+        <section className="containerAbout">
             <h2 className="h2About">Historia de las delicias del hogar</h2>
             <div className="marco">
-                <Image 
-                src={require("../assets/Foods/About/contacto.jpg")}
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    borderRadius: "50px",
-                    boxShadow:" 0 0 20px rgba(0, 0, 0, 0.3)"
-             
-                }}
-                />
+           
+      <Image alt="asd"
+                        src={require("../assets/Foods/About/contacto.jpg")}
+                        layout="responsive"
+                    />
+           
             </div>
             <div className="texto">
-            <h3 className="tittleAbout">Nuestra historia</h3>
-            <p  className="parrafo">Lo de Lopez nace en General Villegas sinendo un lugar con
-            con gastronomía variada como Pizzas, Lomos, Hamburguesas y Media Luna rellena.
-            Nuestra misión es llenar y satisfacer el deseo alimenticio de las familias Villeguenses y alrededores
-            para que en las siguientes genereaciones seamos recordamos como un evento familiar con cariño</p>
+                <h3 className="tittleAbout">Nuestra historia</h3>
+                <p className="parrafo">Lo de Lopez nace en General Villegas sinendo un lugar con
+                    con gastronomía variada como Pizzas, Lomos, Hamburguesas y Media Luna rellena.
+                    Nuestra misión es llenar y satisfacer el deseo alimenticio de las familias Villeguenses y alrededores
+                    para que en las siguientes genereaciones seamos recordamos como un evento familiar con cariño</p>
             </div>
         </section>
     )
